511c6a3c6305882f3db04dbe87b72cede06349dc
guozichun <guotingchaopr@gmail.com>
2024-04-13 00:54:42 +0800

    feat(new feature): :sparkles: add configs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+  const host = configService.get('HOST');
+
+  await app.listen(port, host, () => {
+    console.debug(`🚀 Server ready at http://${host}:${port}`);
+  });
 }
+
 bootstrap();
